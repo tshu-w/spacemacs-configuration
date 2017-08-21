3d6493278cb770ed90cc3fce6ea5692757eb3835
Voleking <volekingsg@gmail.com>
2017-08-21 16:36:21 +0800

    Update snippets

--- snippets/c++-mode/hder.cpp
+++ snippets/c++-mode/hder.cpp
@@ -6,12 +6,6 @@
 #include <bits/stdc++.h>
 
 #define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
-// #define __DEBUG__
-#ifdef __DEBUG__
-#define DEBUG(...) printf(__VA_ARGS__)
-#else
-#define DEBUG(...)
-#endif
 #define filename ""
 #define setfile() freopen(filename".in", "r", stdin); freopen(filename".ans", "w", stdout);
 #define resetfile() freopen("/dev/tty", "r", stdin); freopen("/dev/tty", "w", stdout); system("more " filename".ans");
@@ -20,18 +14,19 @@ using namespace std;
 
 template <typename T>
 inline T sqr(T a) { return a * a;};
+template <typename T>
+void dprint(T begin, T end) {for (auto i = begin; i != end; i++) cerr << (*i) << " "; cerr << "\n";};
 
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
 typedef pair<int, int > Pii;
 
-const int MAX_N = 1e5 + 10;
+const int maxn = 1e5 + 10;
 
 $1
 
-int main()
-{
+int main() {
     $2
     return 0;
 }
