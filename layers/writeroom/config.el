7fd9d9dff6b68093ce4f66330470d3bbc3ead42e
Voleking <volekingsg@gmail.com>
2017-09-02 20:59:12 +0800

    Add writeroom layers

---
+++ layers/writeroom/config.el
@@ -0,0 +1,21 @@
+;;; packages.el --- Org Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2017 Voleking
+;;
+;; Author: Sylvain Benner <volekingsg@gmail.com>
+;; URL: https://github.com/Voleking/spacemacs-configuration
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq writeroom-width 120)
+(setq writeroom-fringes-outside-margins t)
+(setq writeroom-bottom-divider-width 0)
+
+(setq writeroom-mode-line t)
+(setq writeroom-mode-line-toggle-position 'mode-line-format)
+
+(setq writeroom-major-modes '(text-mode prog-mode))
+
+;; (add-hook 'find-file-hook #'writeroom-mode)
---
+++ layers/writeroom/packages.el
@@ -0,0 +1,20 @@
+;;; packages.el --- Org Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2017 Voleking
+;;
+;; Author: Sylvain Benner <volekingsg@gmail.com>
+;; URL: https://github.com/Voleking/spacemacs-configuration
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq writeroom-packages '(writeroom-mode))
+
+(defun writeroom/init-writeroom-mode ()
+  "Initialize writeroom-mode"
+
+  (use-package writeroom-mode
+    :init (spacemacs/set-leader-keys "aw" #'writeroom-mode)
+    (global-writeroom-mode 1))
+)
